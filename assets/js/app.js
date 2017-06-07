68a9a5cdd8ea83dbde2a9189d6d900b1a40cf9a7
marianajane <mariana.musm@gmail.com>
2017-06-07 12:34:20 -0500

    Avance Lyft, versión 0.2.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,13 @@
- $('#splash').ready() {
-        $('#main').load('file.html');
+$(document).ready(cargarPagina);
+
+function cargarPagina() {
+	 $('#index').load('index.html');
         setTimeout(function() {
-            $('#main').ready(function() {
+            $('#index').ready(function() {
                 $('#splash').remove();
-                window.location.href = "file.html";
+                window.location.href = "index.html";
             });
         }, 2000);
-    }
+}
+
+       
